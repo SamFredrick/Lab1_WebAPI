dbfe8a8b024d5cd3b2a22ece65887f0d15a7da1f
SamFreddy <sam.fredrick@ucdenver.edu>
2026-02-07 14:44:35 -0700

    files

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 var server = require("http").createServer();
 
 server.on("request", (request, response) => {
-    var body = [];
-    request.on("data", chunk => {
-        body.push(chunk);
-    });
-    request
-        .on("end", () => {
-            let bodyString = body.concat().toString();
-            console.log(bodyString);
-            response.end(bodyString);
-        })
-        .on("error", () => {
-            response.statusCode = 400;
-            response.end();
-        });
-    response.on("error", err => {
-        console.error(err);
-    });
+  if (request.method !== "POST" || request.url !== "/echo") {
+    response.statusCode = 404;
+    return response.end();
+  }
+
+  var body = [];
+
+  request.on("data", chunk => body.push(chunk));
+
+  request.on("end", () => {
+    let bodyString = body.concat().toString();
+    response.statusCode = 200;
+    response.end(bodyString);
+  });
+
+  request.on("error", () => {
+    response.statusCode = 400;
+    response.end();
+  });
 });
+
 server.listen(process.env.PORT || 8008, () => {
-    console.log("Server listening at 8008");
+  console.log("Server listening on 8008");
 });
 
-module.exports = server; // for testing
-
-//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
+module.exports = server;
